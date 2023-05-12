2b619fe68d38b1dc9f39953fe21d4e9073daf1e6
Angel Quintero <angelqv2014@gmail.com>
2023-05-11 20:22:48 -0400

    separate braking input to its own method

--- Assets/CarSim/WheelController.cs
+++ Assets/CarSim/WheelController.cs
@@ -31,17 +31,39 @@ public class WheelController : MonoBehaviour
         
     }
 
+    void Update()
+    {
+        PlayerInputs(Convert.ToSingle(Input.GetKey(KeyCode.Space)));
+        Steering();
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        Braking(Convert.ToSingle(Input.GetKey(KeyCode.Space)));
+        Braking();
         Aceleration();
-        Steering();
-
+        
         VisualWheelUpdate(FrontWheelColliders[0], FrontWheelTransforms[0]);
         VisualWheelUpdate(FrontWheelColliders[1], FrontWheelTransforms[1]);
         VisualWheelUpdate(RearWheelColliders[0], RearWheelTransforms[0]);
         VisualWheelUpdate(RearWheelColliders[1], RearWheelTransforms[1]);
+    }
+
+    /// <summary>
+    /// changes current braking force based on player input
+    /// </summary>
+    /// <param name="brakeInput"></param>
+    void PlayerInputs(float brakeInput)
+    {
+        //Braking Input
+        if (brakeInput != 0)
+        {
+            currentBrakingForce = brakingForce * brakeInput;
+        }
+        else
+        {
+            currentBrakingForce = 0f;
+        }
     }
 
     void VisualWheelUpdate(WheelCollider col, Transform trans)
@@ -57,21 +79,11 @@ public class WheelController : MonoBehaviour
     }
 
     /// <summary>
-    /// Apply braking torque to all wheels based on player input
+    /// Apply braking torque to all wheels
     /// </summary>
     /// <param name="Input"></param>
-    void Braking(float Input)
+    void Braking()
     {
-        //Braking Input
-        if (Input != 0)
-        {
-            currentBrakingForce = brakingForce * Input;
-        }
-        else
-        {
-            currentBrakingForce = 0f;
-        }
-
         //Apply Braking to all Wheels
         foreach (WheelCollider Wheel in FrontWheelColliders)
         {
@@ -82,8 +94,8 @@ public class WheelController : MonoBehaviour
             Wheel.brakeTorque = currentBrakingForce;
         }
 
-        /*Wheel Brake Test
-        Debug.Log($"Brake Torque Test//  Front Left: {FrontWheelColliders[0].brakeTorque}, Front Right: {FrontWheelColliders[1].brakeTorque};  Rear Left: {RearWheelColliders[0].brakeTorque}, Rear Right: {RearWheelColliders[1].brakeTorque}");*/
+        //Wheel Brake Test
+        Debug.Log($"Brake Torque Test//  Front Left: {FrontWheelColliders[0].brakeTorque}, Front Right: {FrontWheelColliders[1].brakeTorque};  Rear Left: {RearWheelColliders[0].brakeTorque}, Rear Right: {RearWheelColliders[1].brakeTorque}");
     }
 
     void Aceleration()
